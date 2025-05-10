4e74bef38d5412437fd34cc61cf9768d7524e7e6
Zachary Zajdel <zaczajdel213@gmail.com>
2025-05-09 23:03:03 -0500

    feat: add tailwind and shadcn

--- client/pages/dashboard/index.vue
+++ client/pages/dashboard/index.vue
@@ -1,4 +1,19 @@
+<script setup lang="ts">
+	import Button from '@/components/ui/button/Button.vue';
+
+	const colorMode = useColorMode();
+</script>
+
 <template>
-	<h1>Dashboard</h1>
-	<NuxtLink to="/">Home</NuxtLink>
+	<h1 class="text-3xl font-bold">Dashboard</h1>
+
+	<div class="flex flex-col gap-4">
+		<NuxtLink to="/">Home</NuxtLink>
+
+		<div class="flex gap-2">
+			<Button @click="colorMode.preference = 'light'">Light</Button>
+			<Button @click="colorMode.preference = 'dark'">Dark</Button>
+			<Button @click="colorMode.preference = 'system'">System</Button>
+		</div>
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
 
-  srcDir: 'client',
+	srcDir: 'client',
 
-  modules: ['@nuxt/eslint', '@nuxt/image']
-})
+	modules: ['@nuxt/eslint', '@nuxt/image', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+
+	colorMode: {
+		storageKey: 'dark',
+		classSuffix: '',
+	},
+
+	css: ['~/assets/css/main.css'],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './client/components/ui',
+	},
+});
