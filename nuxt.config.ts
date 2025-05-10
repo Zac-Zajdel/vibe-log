ea3ccd83d755fac73c0318b3016593eab2085f2c
Zachary Zajdel <zaczajdel213@gmail.com>
2025-05-09 21:19:05 -0500

    feat: initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  srcDir: 'client',
+
+  modules: ['@nuxt/eslint', '@nuxt/image']
+})
