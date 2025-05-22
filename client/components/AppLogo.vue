2dbbb4fb2f8ffe9cfc44d406faf21e6b042d2a6f
Zachary Zajdel <zaczajdel213@gmail.com>
2025-05-21 23:18:48 -0500

    feat: added logo to sidebar and home page

--- client/components/AppLogo.vue
+++ client/components/AppLogo.vue
@@ -1,12 +1,8 @@
-<script setup lang="ts">
-  import { Trophy } from 'lucide-vue-next';
-</script>
-
 <template>
   <div
-    class="bg-primary text-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md"
+    class="flex aspect-square size-10 items-center justify-center rounded-md"
   >
-    <Trophy :size="18" />
+    <NuxtImg src="/images/logo.svg" alt="Win The Take" width="75" height="75" />
   </div>
   <div class="ml-1 grid flex-1 text-left text-sm">
     <span class="mb-0.5 truncate leading-none font-semibold">Win The Take</span>
--- client/components/landing/NavBar.vue
+++ client/components/landing/NavBar.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="flex items-center justify-between pt-6 pb-3">
-    <h1 class="text-2xl font-semibold">Win The Take</h1>
-    <div class="flex gap-4">
-      <Button>
-        <NuxtLink to="/login">Login</NuxtLink>
-      </Button>
-      <Button variant="outline">
-        <NuxtLink to="/register">Register</NuxtLink>
-      </Button>
+  <div class="flex items-center justify-between pt-5 pb-3">
+    <div class="flex items-center">
+      <NuxtImg src="/images/logo.svg" alt="Win The Take" class="size-22" />
+      <h1 class="text-xl">Win The Take</h1>
     </div>
+    <Button>
+      <NuxtLink to="/login">Login</NuxtLink>
+    </Button>
   </div>
 </template>
