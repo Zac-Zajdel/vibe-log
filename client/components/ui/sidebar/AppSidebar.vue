5642ee173e093aa8d18d266ec2768a0824eb6a5d
Zachary Zajdel <zaczajdel213@gmail.com>
2025-05-24 02:03:22 -0500

    feat: added sidebar sections

--- client/components/ui/sidebar/AppSidebar.vue
+++ client/components/ui/sidebar/AppSidebar.vue
@@ -1,22 +1,43 @@
 <script setup lang="ts">
   import { sidebarOptions } from '@/types/sidebar';
+  import { ChevronDown, ChevronUp, User } from 'lucide-vue-next';
+
+  const { logout } = useSanctumAuth();
+
+  const signOut = async () => {
+    await logout();
+    window.location.reload();
+  };
 </script>
 
 <template>
   <Sidebar collapsible="icon">
-    <SidebarHeader>
+    <!-- Hidden for now on collapse but want to show icon of workspace instead-->
+    <SidebarHeader class="mt-2 group-data-[collapsible=icon]:hidden">
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton class="h-13" as-child>
-            <NuxtLink href="/home">
-              <AppLogo />
-            </NuxtLink>
-          </SidebarMenuButton>
+          <DropdownMenu>
+            <!--eslint-disable-next-line vue/attribute-hyphenation -->
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                Default Workspace
+                <ChevronDown class="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent class="w-48">
+              <DropdownMenuItem>
+                <span>Personal Website</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Work</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </SidebarMenuItem>
       </SidebarMenu>
     </SidebarHeader>
 
-    <SidebarContent class="mt-1">
+    <SidebarContent class="group-data-[collapsible=icon]:mt-1.5">
       <SidebarGroup>
         <SidebarGroupContent>
           <SidebarMenu>
@@ -25,7 +46,7 @@
               :key="option.title"
             >
               <SidebarMenuButton as-child>
-                <NuxtLink :href="option.url">
+                <NuxtLink :to="option.url">
                   <component :is="option.icon" />
                   <span>{{ option.title }}</span>
                 </NuxtLink>
@@ -35,5 +56,30 @@
         </SidebarGroupContent>
       </SidebarGroup>
     </SidebarContent>
+
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <!--eslint-disable-next-line vue/attribute-hyphenation -->
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                <User />
+                Username
+                <ChevronUp class="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="top" class="w-48">
+              <DropdownMenuItem>
+                <NuxtLink to="/profile">Profile</NuxtLink>
+              </DropdownMenuItem>
+              <DropdownMenuItem @click="signOut">
+                <span>Sign out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
   </Sidebar>
 </template>
