414e560117114feb014f3238987a7a59b6358ddc
Zachary Zajdel <zaczajdel213@gmail.com>
2025-06-02 23:50:39 -0500

    feat: setup enhanced button styling

--- client/pages/home/index.vue
+++ client/pages/home/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import type { Breadcrumbs } from '@/types/breadcrumbs';
-  import { HomeIcon } from 'lucide-vue-next';
+  import { ArrowLeftIcon, ArrowRightIcon, HomeIcon } from 'lucide-vue-next';
+  import ButtonEnhanced from '~/components/ui/button/ButtonEnhanced.vue';
 
   useHead({ title: 'Home' });
 
@@ -13,8 +14,22 @@
   <PageWrapper :breadcrumbs="breadcrumbs">
     <div class="flex flex-col gap-4">
       <div class="flex gap-2">
-        <Button @click="colorMode.preference = 'light'">Light</Button>
-        <Button @click="colorMode.preference = 'dark'">Dark</Button>
+        <ButtonEnhanced
+          iconPlacement="left"
+          effect="expandIcon"
+          :icon="ArrowLeftIcon"
+          @click="colorMode.preference = 'light'"
+        >
+          Light
+        </ButtonEnhanced>
+        <ButtonEnhanced
+          iconPlacement="right"
+          effect="expandIcon"
+          :icon="ArrowRightIcon"
+          @click="colorMode.preference = 'dark'"
+        >
+          Dark
+        </ButtonEnhanced>
         <Button @click="colorMode.preference = 'system'">System</Button>
       </div>
     </div>
