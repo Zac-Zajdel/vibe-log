fd6b3f5a4bb96c9c63f2be3dac448414b52c32b5
Zachary Zajdel <zaczajdel213@gmail.com>
2025-06-15 01:48:23 -0500

    feat: slim DTO, sleep utility, PR template, and tanstack form when creating workspaces

--- app/Data/Transfer/User/UserData.php
+++ app/Data/Transfer/User/UserData.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Data\Transfer\User;
 
-use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Concerns\TransformableData;
+use Spatie\LaravelData\Contracts\TransformableData as ContractsTransformableData;
+use Spatie\LaravelData\Dto;
 use Spatie\LaravelData\Optional;
 
-final class UserData extends Data
+final class UserData extends Dto implements ContractsTransformableData
 {
+    use TransformableData;
+
     public function __construct(
         public readonly Optional|string $name,
         public readonly Optional|string $email,
--- app/Data/Transfer/WorkspaceUser/WorkspaceUserData.php
+++ app/Data/Transfer/WorkspaceUser/WorkspaceUserData.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Data\Transfer\WorkspaceUser;
 
-use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Concerns\TransformableData;
+use Spatie\LaravelData\Contracts\TransformableData as ContractsTransformableData;
+use Spatie\LaravelData\Dto;
 use Spatie\LaravelData\Optional;
 
-final class WorkspaceUserData extends Data
+final class WorkspaceUserData extends Dto implements ContractsTransformableData
 {
+    use TransformableData;
+
     public function __construct(
         public readonly Optional|int $workspace_id,
         public readonly Optional|int $user_id,
