1d48033059640951823f0f034159f004327b7439
Zachary Zajdel <zaczajdel213@gmail.com>
2025-06-15 21:03:47 -0500

    feat: collapsable component and dropdown items under workspace section

--- client/components/ui/sidebar/AppSidebar.vue
+++ client/components/ui/sidebar/AppSidebar.vue
@@ -2,6 +2,13 @@
   import AppSidebarFooter from '@/components/ui/sidebar/AppSidebarFooter.vue';
   import AppSidebarHeader from '@/components/ui/sidebar/AppSidebarHeader.vue';
   import { sidebarOptions } from '@/types/sidebar';
+  import { ChevronRight } from 'lucide-vue-next';
+
+  const openStates = ref<Record<string, boolean>>({});
+
+  const toggleItem = (title: string) => {
+    openStates.value[title] = !openStates.value[title];
+  };
 </script>
 
 <template>
@@ -13,17 +20,59 @@
       <SidebarGroup>
         <SidebarGroupContent>
           <SidebarMenu>
-            <SidebarMenuItem
-              v-for="option in sidebarOptions"
-              :key="option.title"
-            >
-              <SidebarMenuButton as-child>
-                <NuxtLink :to="option.url">
-                  <component :is="option.icon" />
-                  <span>{{ option.title }}</span>
-                </NuxtLink>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
+            <template v-for="option in sidebarOptions" :key="option.title">
+              <template v-if="option.subItems">
+                <Collapsible
+                  :open="openStates[option.title]"
+                  class="group/collapsible"
+                  @update:open="toggleItem(option.title)"
+                >
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton asChild>
+                        <div class="flex cursor-pointer justify-between">
+                          <div class="flex items-center">
+                            <component :is="option.icon" :size="17" />
+                            <span class="pl-2">{{ option.title }}</span>
+                          </div>
+                          <ChevronRight
+                            class="transition-transform duration-200 ease-in-out"
+                            :class="
+                              openStates[option.title] ? 'rotate-90' : null
+                            "
+                          />
+                        </div>
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        <SidebarMenuSubItem
+                          v-for="item in option.subItems"
+                          :key="item.title"
+                          class="cursor-pointer py-0.5"
+                        >
+                          <SidebarMenuSubButton>
+                            <NuxtLink :to="item.url" class="flex items-center">
+                              <span>{{ item.title }}</span>
+                            </NuxtLink>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              </template>
+              <template v-else>
+                <SidebarMenuItem>
+                  <SidebarMenuButton as-child>
+                    <NuxtLink :to="option.url">
+                      <component :is="option.icon" />
+                      <span>{{ option.title }}</span>
+                    </NuxtLink>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </template>
+            </template>
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarGroup>
--- client/types/sidebar.ts
+++ client/types/sidebar.ts
@@ -1,11 +1,17 @@
-import { CircleUser, Home, Settings } from 'lucide-vue-next';
+import { Box, CircleUser, Home, Settings } from 'lucide-vue-next';
 
 export type SidebarItems = SidebarItem[];
 
-export interface SidebarItem {
+interface SubItems {
   title: string;
   url: string;
+}
+
+export interface SidebarItem {
+  title: string;
+  url?: string;
   icon: Component;
+  subItems?: SubItems[];
 }
 
 export const sidebarOptions: SidebarItems = [
@@ -18,6 +24,28 @@ export const sidebarOptions: SidebarItems = [
     title: 'Standup',
     url: '/standup',
     icon: CircleUser,
+  },
+  {
+    title: 'Workspace',
+    icon: Box,
+    subItems: [
+      {
+        title: 'General',
+        url: '/workspace/general',
+      },
+      {
+        title: 'Members',
+        url: '/workspace/members',
+      },
+      {
+        title: 'Integrations',
+        url: '/workspace/integrations',
+      },
+      {
+        title: 'Plans',
+        url: '/workspace/plans',
+      },
+    ],
   },
   {
     title: 'Settings',
